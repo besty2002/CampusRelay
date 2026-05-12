f2e48c003e849b79257a50b8710bf5aa6a7e1ef7
besty2002 <doogiya2002@gmail.com>
2026-05-12 23:11:41 +0900

    Recover production by clearing stale PWA state

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { registerSW } from 'virtual:pwa-register'
-
-if (import.meta.env.PROD) {
-  registerSW()
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
